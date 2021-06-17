8fb73e82f88ed9a82004356f344f433fcf1e9d29
RicardoMVA <ricardo-valenca@hotmail.com>
2021-06-17 18:21:30 -0300

    carrega lista de fotos mais recentes na página principal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
 import SearchNavbar from './components/SearchNavbar';
+import getRecent from "./services/flickrApi"
 
 const App : React.FC = () => {
+  useEffect(() => {
+    (async () => {
+      const listaRecentes = await getRecent()
+      console.log(listaRecentes)
+    })()    
+  }, []);
+
   return (
     <div className="App">
       <SearchNavbar />
