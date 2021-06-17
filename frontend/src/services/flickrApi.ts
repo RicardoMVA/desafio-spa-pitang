35eec98827f154002afd5ba375f901df7d96882b
RicardoMVA <ricardo-valenca@hotmail.com>
2021-06-17 18:18:28 -0300

    cria service para consumir api do flickr

---
+++ frontend/src/services/flickrApi.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const getRecent = async () : Promise<any> => {
+  const options = {
+    params: {
+      api_key: '6e31907a124b1f213987edd662bca758',
+      method: 'flickr.photos.getRecent',
+      format: 'json',
+      per_page: 20,
+      page: 1,
+      nojsoncallback: 1
+    }
+  }
+  
+  const response = await axios.get(`https://api.flickr.com/services/rest/`, options)
+  return response.data
+}
+
+export default getRecent
