c138e562c1f91aa899c3a2ee6ff6add73c21eec3
RicardoMVA <ricardo-valenca@hotmail.com>
2021-06-20 12:31:10 -0300

    remove 'adicionados recentemente' - safe search não é segura o suficiente; reimplementa resultado vazio

--- frontend/src/pages/Main/index.tsx
+++ frontend/src/pages/Main/index.tsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import debounce from 'lodash.debounce';
 import Container from "react-bootstrap/Container";
 import SearchNavbar from '../../components/SearchNavbar';
 import GridFotos from '../../components/GridFotos';
-import { getRecent, search } from "../../services/flickrApi"
+import { search } from "../../services/flickrApi"
 import { IFlickrResponse } from '../../interfaces/IFlickrResponse'
 import { IFlickrPhoto } from '../../interfaces/IFlickrPhoto'
 import "./style.css";
@@ -12,33 +12,20 @@ const Main : React.FC = () => {
   const [listaFotos, setListaFotos] = useState<IFlickrPhoto[]>([])
   const [searchedText, setSearchedText] = useState("")
   const [page, setPage] = useState(1)
-  const [descricaoResultado, setDescricaoResultado] = useState("")
-
-  useEffect(() => {
-    runAdicionadosRecentemente()    
-  }, []);
-
-  const runAdicionadosRecentemente = async () => {
-    setSearchedText("")
-    setPage(1)
-
-    const respRecentes: IFlickrResponse = await getRecent(page)
-    const listaRecentes: Array<IFlickrPhoto> = respRecentes.photos.photo
-    setDescricaoResultado("Fotos adicionadas recentemente:")
-    return setListaFotos(listaRecentes)
-  }
+  const [descricaoResultado, setDescricaoResultado] = useState("Pesquise algo para começar.")
 
   const runSearch = async (searchText: string) => {
     setSearchedText(searchText)
     setPage(1)
     const pesquisa: IFlickrResponse = await search(searchText, 1)
 
-    if (pesquisa.stat === "fail") {
-      runAdicionadosRecentemente()
+    if (pesquisa.stat === "fail" || pesquisa.photos.photo.length < 1) {
+      setListaFotos([])
+      return setDescricaoResultado("Nenhum resultado. Por favor, tente outro termo.") 
     } else {
       const resultadoPesquisa: Array<IFlickrPhoto> = pesquisa.photos.photo
-      setDescricaoResultado(`Resultado da pesquisa por '${searchText}':`)
-      return setListaFotos(resultadoPesquisa)
+      setListaFotos(resultadoPesquisa)
+      return setDescricaoResultado(`Resultado da pesquisa por '${searchText}':`)
     }
   }
 
@@ -48,19 +35,14 @@ const Main : React.FC = () => {
     const alturaTotal = document.documentElement.offsetHeight
 
     if (alturaTela + posicao >= Math.round(alturaTotal * 0.8)) {
-      const newPage = page + 1
-      setPage(newPage)
-
-      let fetch: IFlickrResponse;
-
       if (searchedText) {
-        fetch = await search(searchedText, newPage)
-      } else {
-        fetch = await getRecent(newPage)
-      }
+        const newPage = page + 1
+        setPage(newPage)
 
-      const resultado: Array<IFlickrPhoto> = fetch.photos.photo
-      return setListaFotos([...listaFotos, ...resultado])
+        const fetch: IFlickrResponse = await search(searchedText, newPage)
+        const resultado: Array<IFlickrPhoto> = fetch.photos.photo
+        return setListaFotos([...listaFotos, ...resultado])
+      }
     }
   }, 200);
 
@@ -68,7 +50,11 @@ const Main : React.FC = () => {
     <div className="main">
       <SearchNavbar handleSubmit={runSearch} />
       <Container className="main-container-descricao">
-        <h5>{descricaoResultado}</h5>
+        <h5 className="main-descricao">{descricaoResultado}</h5>
+        {searchedText 
+          ? null
+          : (<img src={'./search-image.png'} className="main-background-img" alt="lupa sobre imagem" />)
+        }
       </Container>
       <GridFotos fotos={listaFotos} />
     </div>
